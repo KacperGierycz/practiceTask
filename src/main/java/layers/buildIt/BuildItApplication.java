f8cf17358d0d122c93360465b37868efb7f0e74e
Kacper <kacpergierycz@gmail.com>
2022-04-20 10:45:36 +0200

    project repacked from spring io

---
+++ src/main/java/layers/buildIt/BuildItApplication.java
@@ -0,0 +1,13 @@
+package layers.buildIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/layers/buildIt/BuildItApplicationTests.java
@@ -0,0 +1,13 @@
+package layers.buildIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
